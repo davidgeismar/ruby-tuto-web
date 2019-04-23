c741445c0d9a712dd4eef24968651094c6458fda
David GEISMAR <david.geismar@colossusbets.com>
2019-04-23 14:02:27 +0100

    check resource

--- create_array/create_array.c
+++ create_array/create_array.c
@@ -2,5 +2,6 @@
 
 int * create_static_array(int size) {
   int *arr = malloc(size * sizeof(int));
+  printf("int=%lu bytes\n", sizeof(int));
   return arr;
 }
--- dynamic_array.rb
+++ dynamic_array.rb
@@ -33,7 +33,7 @@ class StaticArray
       i += 1
     end
   end
-  def print
+  def print_at_bit
     i = 0
     while (i < @capacity)
       puts @pointer[i].read(:int)
@@ -68,8 +68,55 @@ class DynamicArray
     @static_array = new_arr
   end
 end
+http://www.cse.cuhk.edu.hk/~taoyf/course/comp3506/lec/dyn-array.pdf
 
- #
- # /\/{2}(\w+\.(com|org))\//
- #
- # begins with //
+# performing amortize analysis on dynamic arrays
+# generally we analyse algorithm with asymptotic analysis
+# asymptotic analysis how time grows when the input of the algorithm grows
+# for example a simple loop algorithm grows linearly as the input grows
+
+# amortized analysis is another way to study time efficiency of an algorithm
+# it is more appropriate in situations where an algorithm has operation that take very little time and happen often (not costly)
+# and 1 operation that takes a lot of time but doesnt happen often
+
+# it is the case with dynamic arrays as most of the time adding an element to the array is a small cheap operation
+#  whereas occasionaly you can have a very costly operation (copying all the elements to a new resized array)
+
+# doing this takes  O(n) time if you apply asymptotic analysis
+# But appends take O(n) time only when we insert into a full array
+#  So in most cases appending is still O(1)O(1) time, and sometimes it's O(n)O(n) time.
+
+# "Amortize" is a fancy verb used in finance that refers to paying off the cost of something gradually.
+# With dynamic arrays, every expensive append where we have to grow the array "buys" us many cheap appends in the future.
+# Conceptually, we can spread the cost of the expensive append over all those cheap appends.
+
+# The cost of doing mm appends is mm (since we're appending every item),
+# plus the cost of any doubling when the dynamic array needs to grow. How much does the doubling cost?
+
+# 1 + 2 + 4 + 8 + .... + m/2 + m
+# So when we do m appends, the appends themselves cost m, and the doubling costs 2m
+#
+# Put together, we've got a cost of 3m, which is O(m). So on average, each individual append is O(1). m appends cost us O(m).
+
+
+
+
+
+-----
+
+# In such cases, it is often too crude to estimate the total time spend by these operations using the number
+# of operations multiplied by the worst-case running time for one operation. This is because maybe only a
+# small fraction of the operations take a long time.
+# Amortized analysis tries to analyze the cost of th
+
+
+-----
+
+# If we use simple analysis, the worst case cost of an insertion is O(n).
+# Therefore, worst case cost of n inserts is n * O(n) which is O(n2).
+# This analysis gives an upper bound, but not a tight upper bound for n insertions as all insertions don’t take Θ(n) time.
+
+
+# https://www.geeksforgeeks.org/analysis-algorithm-set-5-amortized-analysis-introduction/
+
+# check why 1 + 2 + 4 +8 +.....+n/2 + n = log2(n - 1) +1 ???? = 2m
